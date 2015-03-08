b1f1f8112332d81cdc5def4bbaceb1090d66aaec
Sebastian Geiger <sebastian@bignerdranch.com>
2015-03-07 22:48:18 -0500

    Adding ENV['MY_LAT'] to configuration

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ fn main(){
 #[derive(Debug, Eq, PartialEq)]
 struct Configuration {
     api_key: String,
+    lat: String,
 }
 
 #[derive(Debug, Eq, PartialEq)]
@@ -17,9 +18,16 @@ struct ProgramError {
 type ConfigurationResult = Result<Configuration, ProgramError>;
 
 fn read_configuration() -> ConfigurationResult {
-    match env::var("FORECAST_IO_API_KEY") {
-        Ok(api_key) => Ok(Configuration { api_key: api_key }),
-        Err(_) => Err(ProgramError { message: "ENV['FORECAST_IO_API_KEY'] not set".to_string() })
+    let envs = (env::var("FORECAST_IO_API_KEY"), env::var("MY_LAT"));
+    match envs {
+        (Ok(api_key), Ok(lat)) =>
+            Ok(Configuration { api_key: api_key, lat: lat }),
+
+        (Err(_), _) =>
+            Err(ProgramError { message: "ENV['FORECAST_IO_API_KEY'] not set".to_string() }),
+
+        (_, Err(_)) =>
+            Err(ProgramError { message: "ENV['MY_LAT'] not set".to_string() })
     }
 }
 
@@ -32,19 +40,30 @@ mod tests {
 
     #[test]
     fn test_configuration_equality() {
-        let config_1 = Configuration { api_key: "A".to_string() };
-        let config_2 = Configuration { api_key: "A".to_string() };
-        let config_3 = Configuration { api_key: "B".to_string() };
+        let config_1 = Configuration { api_key: "A".to_string(), lat: "33".to_string() };
+        let config_2 = Configuration { api_key: "A".to_string(), lat: "33".to_string() };
+        let config_3 = Configuration { api_key: "A".to_string(), lat: "70".to_string() };
+        let config_4 = Configuration { api_key: "B".to_string(), lat: "33".to_string() };
         assert_eq!(config_1, config_1);
         assert_eq!(config_1, config_2);
         assert!(config_1 != config_3);
+        assert!(config_1 != config_4);
     }
 
     #[test]
     fn test_read_configuration() {
         env::set_var("FORECAST_IO_API_KEY", "123-KEY-456");
-        let config = Configuration { api_key: "123-KEY-456".to_string() };
+        env::set_var("MY_LAT", "33.835297");
+        // env::set_var("MY_LNG", "-84.321231");
+        let config = Configuration {
+            api_key: "123-KEY-456".to_string(),
+            lat: "33.835297".to_string()
+        };
         assert_eq!(read_configuration(), Ok(config));
+
+        env::remove_var("MY_LAT");
+        let error = ProgramError { message: "ENV['MY_LAT'] not set".to_string() };
+        assert_eq!(read_configuration(), Err(error));
 
         env::remove_var("FORECAST_IO_API_KEY");
         let error = ProgramError { message: "ENV['FORECAST_IO_API_KEY'] not set".to_string() };
