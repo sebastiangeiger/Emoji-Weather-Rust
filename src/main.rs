61116aa33f6c6135e24d11410d44ac60db042cd6
Sebastian Geiger <sebastian@bignerdranch.com>
2015-03-07 22:16:14 -0500

    Reading the API key into struct works

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,41 @@
-extern crate hyper;
+use std::env;
+use std::env::VarError;
 
-use std::io::Read;
 
-use hyper::Client;
-use hyper::header::Connection;
-use hyper::header::ConnectionOption;
+#[derive(Debug, Eq, PartialEq)]
+struct Configuration {
+    api_key: String,
+}
 
-fn main() {
-    // Create a client.
-    let mut client = Client::new();
+type ConfigurationResult = Result<Configuration, VarError>;
 
-    // Creating an outgoing request.
-    let mut res = client.get("http://www.gooogle.com/")
-        // set a header
-        .header(Connection(vec![ConnectionOption::Close]))
-        // let 'er go!
-        .send().unwrap();
+fn read_configuration() -> ConfigurationResult {
+    match env::var("FORECAST_IO_API_KEY") {
+        Ok(api_key) => Ok(Configuration { api_key: api_key }),
+        Err(error) => Err(error)
+    }
+}
 
-    // Read the Response.
-    let mut body = String::new();
-    res.read_to_string(&mut body).unwrap();
+#[cfg(test)]
+mod tests {
+    use std::env;
+    use read_configuration;
+    use Configuration;
 
-    println!("Response: {}", body);
+    #[test]
+    fn test_configuration_equality() {
+        let config_1 = Configuration { api_key: "A".to_string() };
+        let config_2 = Configuration { api_key: "A".to_string() };
+        let config_3 = Configuration { api_key: "B".to_string() };
+        assert_eq!(config_1, config_1);
+        assert_eq!(config_1, config_2);
+        assert!(config_1 != config_3);
+    }
+
+    #[test]
+    fn test_read_configuration() {
+        env::set_var("FORECAST_IO_API_KEY", "123-KEY-456");
+        let config = Configuration { api_key: "123-KEY-456".to_string() };
+        assert_eq!(read_configuration(), Ok(config));
+    }
 }
